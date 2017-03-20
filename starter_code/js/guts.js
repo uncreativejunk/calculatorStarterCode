e34d37e8f3b894bf7c36a5f244fd04dd1935e9dd
stacy <smartinez01.student@hsfi.us>
2017-03-20 20:57:54 +0000

    I made this!

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -5,41 +5,47 @@
 
 function divide(x,y)
 {
-	//Write your code below this line
-	
+   if (x===0) {
+     $("h1").append("ERROR");
+    }
+      return x/y;
 }
 
 function multiply(x,y)
 {
-	
+	return x*y;
 }
 
 function subtract(x,y)
 {
-	
+    return x-y;
 }
 
 function add(x,y)
 {
-	
+    return x+y;
 }
 
 function power(x,y)
 {
-	
+    return Math.pow(x, y);
 }
 
 function square_root(x)
 {
-	
+	return Math.sqrt(x);
 }
 
 function logBase10(x)
 {
-	
+	return Math.log10(x);
 }
 
 function generateRandomNumber()
 {
-	
+	return Math.random() *30;
+}
+function zero()
+{
+    return 
 }
